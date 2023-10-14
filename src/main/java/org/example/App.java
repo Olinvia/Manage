aa540171536d2552ef7764ec201df6b03dd6695b
Olinvia <1013581763@qq.com>
2023-10-14 11:29:21 +0800

    create a Java Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = ArithmeticUtils.gcd(3,45);
+        System.out.println(a);
+    }
+
+}
